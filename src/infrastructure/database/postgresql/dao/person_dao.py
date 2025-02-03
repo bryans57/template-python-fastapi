67625923bfd52bd273f16b31568d43fd643d63a6
bryans57 <bryanscm1@gmail.com>
2025-02-03 08:34:32 -0500

    feat: improve person dao
    - Change redundat code
    - Remove posible sql injection

--- src/infrastructure/database/postgresql/dao/person_dao.py
+++ src/infrastructure/database/postgresql/dao/person_dao.py
@@ -1,3 +1,4 @@
+from contextlib import contextmanager
 from typing import List
 
 from injector import inject
@@ -18,105 +19,86 @@ class PersonDAO(PersonRepository):
     def __init__(self, db: Postgresql):
         self.db = db
 
-    def get_info(self, identifications: list, fields_info: list) -> List[Person]:
+    @contextmanager
+    def get_cursor(self):
+        """Context manager to handle database connections safely."""
         con = self.db.get_connection()
         try:
             with con.cursor(cursor_factory=RealDictCursor) as cursor:
-                filtered_fields_info = [field for field in fields_info if field != "identification"]
-                fields_set = sql.SQL(", ").join(map(sql.Identifier, filtered_fields_info))
-
-                query = sql.SQL(
-                    """
-						WITH list_person AS (
-                            SELECT DISTINCT UNNEST(ARRAY [%s]) AS identification
-                        )
-                        SELECT lp.identification, {fields}
-                        FROM list_person lp
-                        LEFT JOIN person p ON lp.identification = p.identification;
-					"""
-                ).format(fields=fields_set)
-
-                cursor.execute(query, (identifications,))
+                yield cursor
                 con.commit()
-
-                results = cursor.fetchall()
-                result_convert = [Person(**fila) for fila in results]
-                return result_convert
         except DatabaseError as e:
+            con.rollback()
             raise DatabaseErrorHandling(e) from e
         finally:
             self.db.put_connection(con)
+
+    def get_info(self, identifications: list, fields_info: list) -> List[Person]:
+        filtered_fields_info = [field for field in fields_info if field != "identification"]
+
+        with self.get_cursor() as cursor:
+            query = sql.SQL(
+                """
+                    WITH list_person AS (
+                        SELECT DISTINCT UNNEST(ARRAY [%s]) AS identification
+                    )
+                    SELECT lp.identification, {fields}
+                    FROM list_person lp
+                    LEFT JOIN person p ON lp.identification = p.identification;
+                """
+            ).format(fields=sql.SQL(", ").join(map(sql.Identifier, filtered_fields_info)))
+
+            cursor.execute(query, (identifications,))
+            results = cursor.fetchall()
+
+            return [Person(**fila) for fila in results]
 
     def add_person(self, person: Person) -> Person:
-        con = self.db.get_connection()
-        try:
-            with con.cursor(cursor_factory=RealDictCursor) as cursor:
-                # Convert person to a dictionary
-                person_data = person.model_dump()
-                fields, values = list(person_data.keys()), list(person_data.values())
+        # Convert person to a dictionary
+        person_data = person.model_dump()
+        fields, values = list(person_data.keys()), list(person_data.values())
 
-                # Create SQL for fields and placeholders
-                fields_set = sql.SQL(", ").join(map(sql.Identifier, fields))
-                placeholders = sql.SQL(", ").join(sql.Placeholder() for _ in fields)
+        with self.get_cursor() as cursor:
+            # Create SQL for fields and placeholders
 
-                query = sql.SQL(
-                    """
-                    INSERT INTO person ({fields})
-                    VALUES ({placeholders})
-                    RETURNING *;
-                    """
-                ).format(fields=fields_set, placeholders=placeholders)
+            query = sql.SQL(
+                """
+                INSERT INTO person ({fields})
+                VALUES ({placeholders})
+                RETURNING *;
+                """
+            ).format(
+                fields=sql.SQL(", ").join(map(sql.Identifier, fields)),
+                placeholders=sql.SQL(", ").join(sql.Placeholder() for _ in fields),
+            )
 
-                # Log query
-                full_query = cursor.mogrify(query.as_string(con), values)
-                print(full_query.decode("utf-8"))
-
-                # Execute query with values
-                cursor.execute(query, values)
-                con.commit()
-                result = cursor.fetchone()
-
-                return Person(**result)
-        except DatabaseError as e:
-            raise DatabaseErrorHandling(e) from e
-        finally:
-            self.db.put_connection(con)
+            # Execute query with values
+            cursor.execute(query, values)
+            result = cursor.fetchone()
+            return Person(**result)
 
     def update_person(self, person: Person) -> Person:
-        con = self.db.get_connection()
-        try:
-            with con.cursor(cursor_factory=RealDictCursor) as cursor:
-                # Convert person to a dictionary
-                person_data = person.model_dump()
-                filtered_person_data = {k: v for k, v in person_data.items() if k != "identification" and v is not None}
-                dict_values_person = filtered_person_data.items()
+        # Convert person to a dictionary
+        person_data = person.model_dump()
+        filtered_person_data = {k: v for k, v in person_data.items() if k != "identification" and v is not None}
 
-                query = sql.SQL(
-                    """
-                    UPDATE person
-                    SET {fields}
-                    WHERE identification = {identification}
-                    RETURNING *;
-                    """
-                ).format(
-                    fields=sql.SQL(", ").join(
-                        sql.Composed([sql.Identifier(field), sql.SQL(" = "), sql.Literal(value)])
-                        for field, value in dict_values_person
-                    ),
-                    identification=sql.Literal(person.identification),
-                )
+        with self.get_cursor() as cursor:
+            fields = sql.SQL(", ").join(
+                sql.Composed([sql.Identifier(field), sql.SQL(" = "), sql.Placeholder(f"{field}")])
+                for field in filtered_person_data.keys()
+            )
 
-                # Log query
-                full_query = cursor.mogrify(query.as_string(con), dict_values_person)
-                print(full_query.decode("utf-8"))
+            query = sql.SQL(
+                """
+                UPDATE person
+                SET {fields}
+                WHERE identification = %(identification)s
+                RETURNING *;
+                """
+            ).format(fields=fields)
 
-                # Execute query with values
-                cursor.execute(query, dict_values_person)
-                con.commit()
-                result = cursor.fetchone()
+            # Execute query with values
+            cursor.execute(query, person_data)
+            result = cursor.fetchone()
 
-                return Person(**result)
-        except DatabaseError as e:
-            raise DatabaseErrorHandling(e) from e
-        finally:
-            self.db.put_connection(con)
+            return Person(**result)
